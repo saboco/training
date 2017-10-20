9918d96ff7f93f9d02fd8c15173f9fe154b392a4
saboco <sabotero@gmail.com>
2017-10-20 15:00:28 +0200

    Boxing

---
+++ Training.Common/Boxing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Training.Common
+{
+    public static class Boxing
+    {
+        public static void Box(int i)
+        {
+            object a = i; // the obvious boxing operation
+
+            // less obvious boxing operations
+            IEquatable<int> b = i;
+            IComparable c = i;
+            dynamic d = i;
+
+            // strictly speaking not a boxing operation (on IL) 
+            // but still there is an allocation on the heap 
+            // and the value is copied to an internal field/property of the class
+            MyBoxingClass e = i;
+
+            Console.WriteLine(i);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+
+    public class MyBoxingClass
+    {
+        private int Value { get; }
+
+        private MyBoxingClass(int i)
+        {
+            Value = i;
+        }
+
+        public static implicit operator MyBoxingClass(int i)
+        {
+            return new MyBoxingClass(i);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
