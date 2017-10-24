4b475f01bfd936c4ea36600c508c4f3662e46f75
saboco <sabotero@gmail.com>
2017-10-24 16:19:13 +0200

    Recursion Davis' Staircase

---
+++ Training.HackerRank.Tests/Techniques_Concepts/RecursionDavisStaircaseTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Training.HackerRank.Techniques_Concepts;
+
+namespace Training.HackerRank.Tests.Techniques_Concepts
+{
+    public class RecursionDavisStaircaseTests
+    {
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 4)]
+        [TestCase(7, ExpectedResult = 44)]
+        public long Should_return_the_number_of_ways_davis_can_clilmb_an_staircase(int staircaseSize)
+        {
+            return RecursionDavisStaircase.GetNumberOfWays(staircaseSize);
+        }
+    }
+}
---
+++ Training.HackerRank/Techniques_Concepts/RecursionDavisStaircase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Training.Common;
+
+namespace Training.HackerRank.Techniques_Concepts
+{
+    public static class RecursionDavisStaircase
+    {
+        public static long GetNumberOfWays(int staircaseSize)
+        {
+            Func<int, long> climb = null;
+            climb = (n) =>
+            {
+                if (n == 0) return 0;
+                if (n == 1) return 1;
+                if (n == 2) return 2;
+                if (n == 3) return 4;
+                
+                return climb(n - 1) + climb(n - 2) + climb(n - 3);
+            };
+
+            climb = climb.Memoize();
+            
+            return climb(staircaseSize);
+        }
+    }
+}
