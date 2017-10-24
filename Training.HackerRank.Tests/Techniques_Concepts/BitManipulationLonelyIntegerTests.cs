ee2d46e71d832494cedcbed18df57e03b2ddaced
saboco <sabotero@gmail.com>
2017-10-24 17:07:06 +0200

    Bit Manipulation: Lonely Integer

---
+++ Training.HackerRank.Tests/Techniques_Concepts/BitManipulationLonelyIntegerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Training.HackerRank.Techniques_Concepts;
+
+namespace Training.HackerRank.Tests.Techniques_Concepts
+{
+    public class BitManipulationLonelyIntegerTests
+    {
+        [TestCase(new []{0,0,1,2,1}, ExpectedResult = 2)]
+        [TestCase(new []{0,0,1,2,1,0,2}, ExpectedResult = 0)]
+        public int Should_return_the_integer_that_does_not_have_a_pair(int[] arr)
+        {
+            return BitManipulationLonelyInteger.GetLonleyInteger(arr);
+        }
+    }
+}
---
+++ Training.HackerRank/Techniques_Concepts/BitManipulationLonelyInteger.cs
@@ -0,0 +1,15 @@
+﻿namespace Training.HackerRank.Techniques_Concepts
+{
+    public static class BitManipulationLonelyInteger
+    {
+        public static int GetLonleyInteger(int[] arr)
+        {
+            var result = arr[0];
+            for (var i = 1; i < arr.Length; i++)
+            {
+                result ^= arr[i];
+            }
+            return result;
+        }
+    }
+}
