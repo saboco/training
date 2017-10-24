61a91182f653de19588d3c16c8427f7a0a454d27
saboco <sabotero@gmail.com>
2017-10-25 01:18:27 +0200

    Dynamic Programming Coin Change

--- Training.Common/FuncExtentions.cs
+++ Training.Common/FuncExtentions.cs
@@ -5,14 +5,13 @@ namespace Training.Common
 {
     public static class FuncExtentions
     {
-        public static Func<T1, T2> Memoize<T1, T2>(this Func<T1, T2> func)
+        public static Func<T1, TResult> Memoize<T1, TResult>(this Func<T1, TResult> func)
         {
-            var dic = new Dictionary<T1, T2>();
-            
+            var dic = new Dictionary<T1, TResult>();
+
             return (T1 n) =>
             {
-                T2 result;
-                if (dic.TryGetValue(n, out result)) return result;
+                if (dic.TryGetValue(n, out var result)) return result;
 
                 result = func(n);
                 dic.Add(n, result);
---
+++ Training.HackerRank/Techniques_Concepts/DynamicProgrammingCoinChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Training.HackerRank.Techniques_Concepts
+{
+    public static class DynamicProgrammingCoinChange
+    {
+        public static long GetWaysOfMakeChange(int amount, int[] coins, Dictionary<string, long> memo)
+        {
+            if (amount == 0) return 1;
+            if (amount < 0) return 0;
+            if (coins.Length == 0) return 0;
+
+            var key = string.Concat(amount, "¤", string.Join("¤", coins));
+            if (memo.ContainsKey(key)) return memo[key];
+
+            var result = GetWaysOfMakeChange(amount - coins[0], coins, memo)
+                         + GetWaysOfMakeChange(amount, GetCoinsWithoutFirstKind(coins), memo);
+
+            memo.Add(key, result);
+            return memo[key];
+        }
+
+        private static int[] GetCoinsWithoutFirstKind(int[] coins)
+        {
+            var newCoins = new int[coins.Length - 1];
+            for (var i = 0; i < newCoins.Length; i++)
+            {
+                newCoins[i] = coins[i + 1];
+            }
+            return newCoins;
+        }
+    }
+}
