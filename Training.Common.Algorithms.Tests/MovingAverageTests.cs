cf01447fd133f9ced49922fe8f668a1e9ddb2ee3
saboco <sabotero@gmail.com>
2017-10-27 19:02:12 +0200

    Moving average in an array

---
+++ Training.Common.Algorithms.Tests/MovingAverageTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Training.Common.Algorithms.Tests
+{
+    public class MovingAverageTests
+    {
+        [TestCase(4, new[] {3d, 5d, 2d, 8d, 1d, 2d, 9d, 1.5d, 7.5d, 5.5d}, new[] {4.5d, 4d, 3.25d, 5d, 3.375d, 5d, 5.875d})]
+        public void Should_retun_the_moving_average_for_the_given_array_in_a_k_window(int k, double[] arr, double[] expected)
+        {
+            var actual = MovingAverage.GetMovingAverage(arr, k);
+            var i = 0;
+            foreach (var d in actual)
+            {
+                Assert.AreEqual(expected[i], d, 0.1);
+                i++;
+            }
+        }
+    }
+}
---
+++ Training.Common.Algorithms/MovingAverage.cs
@@ -0,0 +1,32 @@
+﻿namespace Training.Common.Algorithms
+{
+    public static class MovingAverage
+    {
+        public static double[] GetMovingAverage(double[] arr, int k)
+        {
+            var movingAverage = new double[arr.Length - k + 1];
+            
+            var j = 0;
+            var sum = 0d;
+            for (var i = 0; i < k; i++)
+            {
+                sum += arr[i];
+            }
+            var lastV = arr[k-1];
+            foreach (var v in arr)
+            {
+                var nextIndex = j + k - 1;
+                if (nextIndex >= arr.Length) break;
+                var nextV = arr[nextIndex];
+                
+                sum -= lastV;
+                sum += nextV;
+                movingAverage[j++] = sum / k;
+                
+                lastV = v;
+            }
+
+            return movingAverage;
+        }
+    }
+}
