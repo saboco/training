29328d16b7285c6d12fd92a88c257edd11cffdfd
saboco <sabotero@gmail.com>
2017-10-30 19:28:47 +0100

    The Power Sum

---
+++ Training.HackerRank.Tests/Algorithms/ThePowerSumTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Training.HackerRank.Algorithms;
+
+namespace Training.HackerRank.Tests.Algorithms
+{
+    public class ThePowerSumTests
+    {
+        [Ignore("Not implemented")]
+        [TestCase(10, 2, ExpectedResult = 1)]
+        [TestCase(100, 2, ExpectedResult = 2)]
+        public long Should_return_the_number_of_ways_x_can_be_represented_as_powers_of_n(int x, int n)
+        {
+            return ThePowerSum.GetNumberOfRepresentationsFor(x, n);
+        }
+    }
+}
---
+++ Training.HackerRank/Algorithms/ThePowerSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Training.HackerRank.Algorithms
+{
+    public static class ThePowerSum
+    {
+        public static long GetNumberOfRepresentationsFor(int x, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
