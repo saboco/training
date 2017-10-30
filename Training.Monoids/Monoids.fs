366469950b275dc305e707a2afad9668de36de27
saboco <sabotero@gmail.com>
2017-10-30 19:28:54 +0100

    Monoids

---
+++ Training.Monoids/Monoids.fs
@@ -0,0 +1,50 @@
+﻿namespace Training.Monoids
+
+    module WordCountTest = 
+        open System
+
+        type Text = Text of string
+    
+        let addText (Text s1) (Text s2) =
+            Text (s1 + s2)
+    
+        let wordCount (Text s) =
+            System.Text.RegularExpressions.Regex.Matches(s,@"\S+").Count
+            
+        let page() = 
+            List.replicate 1000 "hello "
+            |> List.reduce (+)
+            |> Text
+
+        let document() = 
+            page() |> List.replicate 1000 
+
+
+        let time f msg = 
+            let stopwatch = Diagnostics.Stopwatch()
+            stopwatch.Start()
+            f() 
+            stopwatch.Stop()
+            printfn "Time taken for %s was %ims" msg stopwatch.ElapsedMilliseconds
+
+
+        let wordCountViaAddText() = 
+            document() 
+            |> List.reduce addText
+            |> wordCount
+            |> printfn "The word count is %i"
+
+            
+        let wordCountViaParallelAddCounts() = 
+            document() 
+            |> List.toArray
+            |> Array.Parallel.map wordCount
+            |> Array.reduce (+)
+            |> printfn "The word count is %i"
+
+        
+        let wordCountViaMap() = 
+            document() 
+            |> List.map wordCount
+            |> List.reduce (+)
+            |> printfn "The word count is %i"
---
+++ Training.Monoids/Monoids.fsx
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Monoids.fs"
+open Training.Monoids.WordCountTest
+
+
+time wordCountViaAddText "reduce then count"
+time wordCountViaMap "map then reduce"
+time wordCountViaParallelAddCounts "parallel map then reduce"
