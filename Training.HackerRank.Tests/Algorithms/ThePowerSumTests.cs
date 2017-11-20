c4ac7198171829598081fe0b2cbde464a3404ef5
saboco <sabotero@gmail.com>
2017-11-20 15:14:27 +0100

    The Power Sum

--- Training.HackerRank.Tests/Algorithms/ThePowerSumTests.cs
+++ Training.HackerRank.Tests/Algorithms/ThePowerSumTests.cs
@@ -5,9 +5,11 @@ namespace Training.HackerRank.Tests.Algorithms
 {
     public class ThePowerSumTests
     {
-        [Ignore("Not implemented")]
         [TestCase(10, 2, ExpectedResult = 1)]
-        [TestCase(100, 2, ExpectedResult = 2)]
+        [TestCase(100, 2, ExpectedResult = 3)]
+        [TestCase(1, 2, ExpectedResult = 1)]
+        [TestCase(3, 2, ExpectedResult = 0)]
+        [TestCase(4, 2, ExpectedResult = 1)]
         public long Should_return_the_number_of_ways_x_can_be_represented_as_powers_of_n(int x, int n)
         {
             return ThePowerSum.GetNumberOfRepresentationsFor(x, n);
--- Training.HackerRank/Algorithms/ThePowerSum.cs
+++ Training.HackerRank/Algorithms/ThePowerSum.cs
@@ -6,7 +6,18 @@ namespace Training.HackerRank.Algorithms
     {
         public static long GetNumberOfRepresentationsFor(int x, int n)
         {
-            throw new NotImplementedException();
+            return GetNumberOfRepresentationsFor(x, n, 1);
+        }
+
+        public static long GetNumberOfRepresentationsFor(int x, int n, int i)
+        {
+            var currentPower = (int) Math.Pow(i, n);
+            if (currentPower < x)
+            {
+                return GetNumberOfRepresentationsFor(x, n, i + 1)
+                       + GetNumberOfRepresentationsFor(x - currentPower, n, i + 1);
+            }
+            return currentPower == x ? 1 : 0;
         }
     }
 }
