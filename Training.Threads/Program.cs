48853389f9784887ebd6444d8284d33e05f8f4ff
saboco <sabotero@gmail.com>
2017-11-29 18:48:35 +0100

    Threads training

---
+++ Training.Threads/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Training.Threads
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // give this
+            for (var i = 1; i <= 4; i++)
+            {
+                var t = new Thread(delegate() { Console.Write(i); });
+                t.Start();
+            }
+            // find the bug and corret it:
+
+            for (var i = 1; i <= 4; i++)
+            {
+                var j = i; // should make a closure to corret the bug in the code above
+                var t = new Thread(delegate() { Console.Write(j); });
+                t.Start();
+            }
+        }
+    }
+}
