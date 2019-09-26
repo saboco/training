2b3c122b023fbc485bc3cc47db63a4b339504f86
Santiago Botero Correa <sabotero@gmail.com>
2019-09-26 18:13:31 +0200

    Adding greatest common divisor algorithm and tests

--- Algorithms/Algorithms.Tests/ModularArithmeticTests.fs
+++ Algorithms/Algorithms.Tests/ModularArithmeticTests.fs
@@ -9,24 +9,24 @@ module ModularArithmeticTests =
 
     [<Theory>]
     [<InlineData(1, 2)>]
-    [<InlineData(4, 2)>]
-    [<InlineData(10, 3)>]
-    [<InlineData(0, Int32.MaxValue)>]
-    [<InlineData(Int32.MinValue, 0)>]
-    [<InlineData(Int32.MinValue, Int32.MaxValue)>]
+    [<InlineData(2, 4)>]
+    [<InlineData(3, 10)>]
+    [<InlineData(1, Int32.MaxValue)>]
+    [<InlineData(25, 7891)>]
+    [<InlineData(48, Int32.MaxValue)>]
     let ``gcd should fail if a < b`` a b =
         (fun () -> gcd a b |> ignore)
         |> should throw typeof<ArgumentException>
 
 
     [<Theory>]
-    [<InlineData(1, 2, 0)>]
-    [<InlineData(4, 2, 0)>]
+    [<InlineData(2, 1, 1)>]
+    [<InlineData(4, 2, 2)>]
     [<InlineData(10, 3, 1)>]
-    [<InlineData(0, Int32.MaxValue)>]
-    [<InlineData(Int32.MinValue, 0)>]
-    [<InlineData(Int32.MinValue, Int32.MaxValue)>]
-    let ``gcd founds the correct greates common divisor`` b a expected =
+    [<InlineData(0, Int32.MaxValue, Int32.MaxValue)>]
+    [<InlineData(Int32.MinValue, 0, Int32.MinValue)>]
+    [<InlineData(Int32.MaxValue, 78, 1)>]
+    let ``gcd finds the correct greates common divisor`` a b expected =
         gcd a b
         |> should equal expected
         
--- Algorithms/Algorithms/ModularArithmetic.fs
+++ Algorithms/Algorithms/ModularArithmetic.fs
@@ -11,7 +11,9 @@ module ``Euclid's algorithm for greates common divisor`` =
             So gcd(k,0) = gcd(0, k) =k. 
             However, gcd(0,0) isn’t defined. 
             All integers are common divisors of 0 and 0, so there is no greatest one. *)
-        | 0, k | k, 0 -> k 
-        | 0 , 0 -> invalidOp "greates comon divisor is not defined for a=0 and b=0"
+        | 0, k | k, 0 -> k
+        | a, b when (a < 0 || b < 0) -> 
+           invalidOp "not defined for (a or b) < 0"
+        | 0, 0 -> invalidOp "greates comon divisor is not defined for a=0 and b=0"
         | a, b when b > a -> invalidArg "a" "a should be greater or equal than b"
         | a, b -> gcd b (a % b)
