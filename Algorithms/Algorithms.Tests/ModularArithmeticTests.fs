5aca11ab0754816175a889f573d1f2ac18c9ac11
Santiago Botero Correa <sabotero@gmail.com>
2019-09-27 00:24:20 +0200

    eucli extended

--- Algorithms/Algorithms.Tests/ModularArithmeticTests.fs
+++ Algorithms/Algorithms.Tests/ModularArithmeticTests.fs
@@ -14,8 +14,8 @@ module ModularArithmeticTests =
     [<InlineData(1, Int32.MaxValue)>]
     [<InlineData(25, 7891)>]
     [<InlineData(48, Int32.MaxValue)>]
-    let ``gcd should fail if a < b`` a b =
-        (fun () -> gcd a b |> ignore)
+    let ``euclid should fail if a < b`` a b =
+        (fun () -> euclid a b |> ignore)
         |> should throw typeof<ArgumentException>
 
 
@@ -26,7 +26,33 @@ module ModularArithmeticTests =
     [<InlineData(0, Int32.MaxValue, Int32.MaxValue)>]
     [<InlineData(Int32.MinValue, 0, Int32.MinValue)>]
     [<InlineData(Int32.MaxValue, 78, 1)>]
-    let ``gcd finds the correct greates common divisor`` a b expected =
-        gcd a b
+    let ``euclid finds the correct greates common divisor`` a b expected =
+        euclid a b
         |> should equal expected
         
+    open System.Collections.Generic
+    open System.Collections
+            
+    type Data() =
+
+        member __.Collection with get() = 
+            let arr= ResizeArray<obj[]>()
+            arr.Add([|2; 1; (0,1,1)|])
+            arr.Add([|13; 4; (1,-3,1)|])
+            arr.Add([|25; 11; (4, -9, 1)|])
+            arr :> IEnumerable<obj[]>
+        
+        interface IEnumerable<obj[]> with
+            member this.GetEnumerator() =
+              this.Collection.GetEnumerator()
+        
+        interface IEnumerable with
+            member this.GetEnumerator() =
+              upcast this.Collection.GetEnumerator()
+
+    [<Theory>]
+    [<ClassData(typeof<Data>)>]
+    let ``euclid-extended finds the correct greates common divisor and x and y factors so d= ax + by`` a b expected =
+        
+        ``euclid-extended`` a b
+        |> should equal expected
--- Algorithms/Algorithms/ModularArithmetic.fs
+++ Algorithms/Algorithms/ModularArithmetic.fs
@@ -2,7 +2,7 @@
 
 module ``Euclid's algorithm for greates common divisor`` =
 
-    let rec gcd a b =
+    let rec euclid a b =
         match a, b with
         (* From http://mfleck.cs.illinois.edu/building-blocks/version-1.0/number-theory.pdf
         
@@ -16,4 +16,17 @@ module ``Euclid's algorithm for greates common divisor`` =
            invalidOp "not defined for (a or b) < 0"
         | 0, 0 -> invalidOp "greates comon divisor is not defined for a=0 and b=0"
         | a, b when b > a -> invalidArg "a" "a should be greater or equal than b"
-        | a, b -> gcd b (a % b)
+        | a, b -> euclid b (a % b)
+
+    let rec ``euclid-extended`` a b =
+        match a, b with
+        | 0, k 
+        | k, 0 -> (1, 0, k)
+        | a, b when (a < 0 || b < 0) -> 
+           invalidOp "not defined for (a or b) < 0"
+        | 0, 0 -> invalidOp "greates comon divisor is not defined for a=0 and b=0"
+        | a, b when b > a -> invalidArg "a" "a should be greater or equal than b"
+        | a, b -> 
+            let r = (a % b)
+            let (x', y', d) = ``euclid-extended`` b r
+            y', x' - (a/b)*y', d
