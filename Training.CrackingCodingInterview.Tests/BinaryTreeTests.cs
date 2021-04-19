0c5235c894446a5353ec7cc307d9ceb0120a5205
Santiago Botero Correa <sabotero@gmail.com>
2021-04-19 23:11:03 +0200

    basic trees

---
+++ Training.CrackingCodingInterview.Tests/BinaryTreeTests.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BNode = Training.CrackingCodingInterview.BinaryTree.Node;
+
+namespace Training.CrackingCodingInterview.Tests
+{
+    public class BinaryTreeTests
+    {
+        [Fact]
+        public void BinaryTreeTest()
+        {
+            var n0 = new BNode(0);
+            var n1 = new BNode(1);
+            var n2 = new BNode(2);
+            var n3 = new BNode(3);
+            var n4 = new BNode(4);
+            var n5 = new BNode(5);
+            var n6 = new BNode(6);
+            var n7 = new BNode(7);
+            var n8 = new BNode(8);
+            var n9 = new BNode(9);
+
+            n3.Left = n1;
+            n3.Right = n2;
+            n8.Left = n3;
+            n8.Right = n4;
+            n7.Left = n8;
+            n7.Right = n6;
+            n6.Left = n9;
+            n6.Right = n0;
+            n9.Left = n5;
+
+            var tree = new BinaryTree(n7);
+
+            var sb = new StringBuilder();
+            tree.InOrderTraverse(s => sb.Append(s.Data));
+            Assert.Equal("1328475960", sb.ToString());
+            sb.Clear();
+            tree.PreOrderTraverse(s => sb.Append(s.Data));
+            Assert.Equal("7831246950", sb.ToString());
+            sb.Clear();
+            tree.PostOrderTraverse(s => sb.Append(s.Data));
+            Assert.Equal("1234859067", sb.ToString());
+        }
+
+        [Fact]
+        public void BinarySearchTreeTest1()
+        {
+            var nodes = new List<BNode>
+            {
+                new BNode(30),
+                new BNode(20),
+                new BNode(40),
+                new BNode(70),
+                new BNode(60),
+                new BNode(80)
+            };
+
+            var root = new BNode(50);
+
+            var bst = new BinarySearchTree(root);
+            foreach(var node in nodes)
+            { 
+                bst.Insert(node.Data);
+            }
+
+            var sb = new StringBuilder("|");
+            bst.InOrderTraverse(n => sb.Append($"{n.Data}|"));
+            Assert.Equal("|20|30|40|50|60|70|80|", sb.ToString());
+        }
+
+        [Fact]
+        public void BinarySearchTreeTest2()
+        {
+            var nodes = new List<BNode>
+            {
+                new BNode(0),
+                new BNode(1),
+                new BNode(2),
+                new BNode(3),
+                new BNode(4),
+                new BNode(5),
+                new BNode(6),
+                new BNode(8),
+                new BNode(9)
+            };
+
+            var root = new BNode(7);
+
+            var bst = new BinarySearchTree(root);
+            foreach(var node in nodes)
+            { 
+                bst.Insert(node.Data);
+            }
+
+            var sb = new StringBuilder();
+            bst.InOrderTraverse(n => sb.Append(n.Data));
+            Assert.Equal("0123456789", sb.ToString());
+        }
+
+        [Fact]
+        public void BinarySearchTreeInOrderTraversalWithStack()
+        {
+            var nodes = new List<BNode>
+            {
+                new BNode(30),
+                new BNode(20),
+                new BNode(40),
+                new BNode(70),
+                new BNode(60),
+                new BNode(80)
+            };
+
+            var root = new BNode(50);
+
+            var bst = new BinarySearchTree(root);
+            foreach(var node in nodes)
+            { 
+                bst.Insert(node.Data);
+            }
+
+            var sb = new StringBuilder("|");
+            bst.InOrderTraverseWithStack(n => sb.Append($"{n.Data}|"));
+            Assert.Equal("|20|30|40|50|60|70|80|", sb.ToString());
+        }
+
+        [Fact]
+        public void BinaryTreeTraversalsWithStack()
+        {
+            var n0 = new BNode(0);
+            var n1 = new BNode(1);
+            var n2 = new BNode(2);
+            var n3 = new BNode(3);
+            var n4 = new BNode(4);
+            var n5 = new BNode(5);
+            var n6 = new BNode(6);
+            var n7 = new BNode(7);
+            var n8 = new BNode(8);
+            var n9 = new BNode(9);
+
+            n3.Left = n1;
+            n3.Right = n2;
+            n8.Left = n3;
+            n8.Right = n4;
+            n7.Left = n8;
+            n7.Right = n6;
+            n6.Left = n9;
+            n6.Right = n0;
+            n9.Left = n5;
+
+            var tree = new BinaryTree(n7);
+
+            var sb = new StringBuilder();
+            //tree.InOrderTraverseWithStack(s => sb.Append(s.Data));
+            //Assert.Equal("1328475960", sb.ToString());
+            //sb.Clear();
+            //tree.PreOrderTraverseWithStack(s => sb.Append(s.Data));
+            //Assert.Equal("7831246950", sb.ToString());
+            //sb.Clear();
+            tree.PostOrderTraverseWithStack(s => sb.Append(s.Data));
+            Assert.Equal("1234859067", sb.ToString());
+        }
+    }
+}
---
+++ Training.CrackingCodingInterview/BinaryTree.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace Training.CrackingCodingInterview
+{
+    public class BinaryTree
+    {
+        public class Node
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int Data { get; }
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+        public Node Root { get; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+        public void InOrderTraverse(Action<Node> f)
+        {
+            InOrderTraverse(Root, f);
+        }
+
+        private void InOrderTraverse(Node n, Action<Node> f)
+        {
+            if (n == null)
+            { return; }
+            InOrderTraverse(n.Left, f);
+            f(n);
+            InOrderTraverse(n.Right, f);
+        }
+
+        public void InOrderTraverseWithStack(Action<Node> f)
+        {
+            var current = Root;
+            var s = new Stack<Node>();
+            while (current != null || !s.Empty)
+            {
+                while (current != null)
+                {
+                    s.Push(current);
+                    current = current.Left;
+                }
+
+                current = s.Pop();
+                f(current);
+                current = current.Right;
+            }
+        }
+
+
+        public void PreOrderTraverse(Action<Node> f)
+        {
+            PreOrderTraverse(Root, f);
+        }
+
+        private void PreOrderTraverse(Node n, Action<Node> f)
+        {
+            if (n == null)
+            { return; }
+            f(n);
+            PreOrderTraverse(n.Left, f);
+            PreOrderTraverse(n.Right, f);
+        }
+
+        public void PreOrderTraverseWithStack(Action<Node> f)
+        {
+            var current = Root;
+            var s = new Stack<Node>();
+            while (current != null || !s.Empty)
+            {
+
+                while (current != null)
+                {
+                    f(current);
+                    s.Push(current.Right);
+                    current = current.Left;
+                }
+
+                current = s.Pop();
+            }
+        }
+
+        public void PostOrderTraverse(Action<Node> f)
+        {
+            PostOrderTraverse(Root, f);
+        }
+
+        private void PostOrderTraverse(Node n, Action<Node> f)
+        {
+            if (n == null)
+            { return; }
+            PostOrderTraverse(n.Left, f);
+            PostOrderTraverse(n.Right, f);
+            f(n);
+        }
+
+        public void PostOrderTraverseWithStack(Action<Node> f)
+        {
+            var current = Root;
+            var s = new Stack<Node>();
+            while (current != null || !s.Empty)
+            {
+                while (current != null)
+                {
+                    if(current.Right != null)
+                    { s.Push(current.Right); }
+                    s.Push(current);
+                    current=current.Left;
+                }
+
+                current = s.Pop();
+                if(current.Right!= null && !s.Empty && current.Right== s.Peek())
+                { 
+                    s.Pop();
+                    s.Push(current);
+                    current = current.Right;
+                }
+                else
+                { 
+                    f(current);
+                    current=null;
+                }
+            } 
+        }
+    }
+
+    public class BinarySearchTree : BinaryTree
+    {
+        public BinarySearchTree(Node root) : base(root) { }
+
+        public Node Insert(int val)
+        {
+            return Insert(Root, val);
+        }
+        private Node Insert(Node root, int val)
+        {
+            if (root == null)
+            {
+                return new Node(val);
+            }
+
+            if (root.Data < val)
+            {
+                root.Right = Insert(root.Right, val);
+            }
+            else if (root.Data > val)
+            {
+                root.Left = Insert(root.Left, val);
+            }
+            return root;
+        }
+    }
+}
