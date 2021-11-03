56210a544bdcebc96638b8cdbdb64753e1cf0612
Santiago Botero Correa <sabotero@gmail.com>
2021-11-03 20:35:52 +0100

    some dijkstra variants

--- Training.Common.Algorithms.Tests/DijkstraShortestPathTests.cs
+++ Training.Common.Algorithms.Tests/DijkstraShortestPathTests.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Training.Common.Algorithms.Tests
 {
     public class DijkstraShortestPathTests
     {
+        List<Func<(int, int)[][], int, int, int[]>> _actions = new List<Func<(int, int)[][], int, int, int[]>>
+        {
+            DijkstraShortestPath.ShortestPath,
+            DijkstraShortestPath.ShortestPathOptimization1,
+            DijkstraShortestPath.ShortestPathOptimization2
+        };
+
         [Theory]
         [InlineData(
             new[] { 0, 3, 1, 4, 7 },
-            5, 0,
+            5, 0, 4,
             new[] { 1, 2 },
             new[] { 3 },
             new[] { 1, 3 },
@@ -23,7 +27,40 @@ namespace Training.Common.Algorithms.Tests
             new[] { 2, 5 },
             new[] { 3 },
             new int[0])]
-        public void ShortesPathTest(int[] expected, int n, int start, params int[][] g)
+        public void ShortesPathTest(int[] expected, int n, int start, int end, params int[][] g)
+        {
+            var graph = GetGraph(n, g);
+            foreach (var action in _actions)
+            {
+                var shortestPath = action.Invoke(graph, start, end);
+                Common.AssertEqual(expected, shortestPath);
+            }
+        }
+
+        [Theory]
+        [InlineData(
+            new[] { 0, 3, 1, 4, 7 },
+            new[] { 0, 2, 1, 3, 4 },
+            5, 0, 4,
+            new[] { 1, 2 },
+            new[] { 3 },
+            new[] { 1, 3 },
+            new[] { 4 },
+            new int[0],
+            new[] { 4, 1 },
+            new[] { 1 },
+            new[] { 2, 5 },
+            new[] { 3 },
+            new int[0])]
+        public void ShortesPathWithStepsTest(int[] expectedDistances, int[] expectedPath, int n, int start, int end, params int[][] g)
+        {
+            var graph = GetGraph(n, g);
+            var (distances, path) = DijkstraShortestPath.ShortestPathWithSteps(graph, start, end);
+            Common.AssertEqual(expectedDistances, distances);
+            Common.AssertEqual(expectedPath, path);
+        }
+
+        private (int, int)[][] GetGraph(int n, int[][] g)
         {
             var nodes = new List<int[]>();
             var distances = new List<int[]>();
@@ -46,9 +83,7 @@ namespace Training.Common.Algorithms.Tests
                 }
                 zipped.Add(zip.ToArray());
             }
-
-            var shortestPath = DijkstraShortestPath.ShortestPath(zipped.ToArray(), start);
-            Common.AssertEqual(expected, shortestPath);
+            return zipped.ToArray();
         }
     }
 }
--- Training.Common.Algorithms/DijkstraShortestPath.cs
+++ Training.Common.Algorithms/DijkstraShortestPath.cs
@@ -8,15 +8,13 @@ namespace Training.Common.Algorithms
 {
     public class DijkstraShortestPath
     {
-        public static int[] ShortestPath((int, int)[][] g, int start)
+        public static int[] ShortestPath((int, int)[][] g, int start, int end)
         {
             var q = new PriorityQueue<(int, int)>((parent, n) => n.Item2 < parent.Item2);
             var distances = new int[g.Length];
             var visited = new bool[g.Length];
-            for (var i = 0; i < distances.Length; i++)
-            {
-                distances[i] = Int32.MaxValue;
-            }
+            Array.Fill(distances, Int32.MaxValue);
+
             distances[start] = 0;
             q.Enqueue((start, 0));
 
@@ -39,6 +37,137 @@ namespace Training.Common.Algorithms
                 }
             }
             return distances;
+        }
+
+        public static int[] ShortestPathOptimization1((int, int)[][] g, int start, int end)
+        {
+            var q = new PriorityQueue<(int, int)>((parent, n) => n.Item2 < parent.Item2);
+            var distances = new int[g.Length];
+            var visited = new bool[g.Length];
+            Array.Fill(distances, Int32.MaxValue);
+
+            distances[start] = 0;
+            q.Enqueue((start, 0));
+
+            while (q.Count > 0)
+            {
+                var (from, currentDistance) = q.Dequeue();
+                visited[from] = true;
+
+                if (distances[from] < currentDistance)
+                { continue; }
+
+                foreach (var neighbor in g[from])
+                {
+                    var (to, weight) = neighbor;
+                    if (visited[to])
+                    { continue; }
+
+                    var newDistance = distances[from] + weight;
+                    if (newDistance < distances[to])
+                    {
+                        distances[to] = newDistance;
+                        q.Enqueue((to, newDistance));
+                    }
+                }
+            }
+            return distances;
+        }
+
+        public static int[] ShortestPathOptimization2((int, int)[][] g, int start, int end)
+        {
+            var q = new PriorityQueue<(int, int)>((parent, n) => n.Item2 < parent.Item2);
+            var distances = new int[g.Length];
+            var visited = new bool[g.Length];
+            Array.Fill(distances, Int32.MaxValue);
+
+            distances[start] = 0;
+            q.Enqueue((start, 0));
+
+            while (q.Count > 0)
+            {
+                var (from, currentDistance) = q.Dequeue();
+                visited[from] = true;
+
+                if (distances[from] < currentDistance)
+                { continue; }
+
+                foreach (var neighbor in g[from])
+                {
+                    var (to, weight) = neighbor;
+                    if (visited[to])
+                    { continue; }
+
+                    var newDistance = distances[from] + weight;
+                    if (newDistance < distances[to])
+                    {
+                        distances[to] = newDistance;
+                        q.Enqueue((to, newDistance));
+                    }
+                }
+                if(from == end)
+                { 
+                    break;
+                }
+            }
+            return distances;
+        }
+
+        public static (int[], int[]) ShortestPathWithSteps((int, int)[][] g, int start, int end)
+        {
+            var q = new PriorityQueue<(int, int)>((parent, n) => n.Item2 < parent.Item2);
+            var visited = new bool[g.Length];
+            var distances = new int[g.Length];
+            var prev = new int[g.Length];
+            Array.Fill(prev, -1);
+            Array.Fill(distances, Int32.MaxValue);
+
+            distances[start] = 0;
+            q.Enqueue((start, 0));
+
+            while (q.Count > 0)
+            {
+                var (from, currentDistance) = q.Dequeue();
+                visited[from] = true;
+
+                if (distances[from] < currentDistance)
+                { continue; }
+
+                foreach (var neighbor in g[from])
+                {
+                    var (to, weight) = neighbor;
+                    if (visited[to])
+                    { continue; }
+
+                    var newDistance = distances[from] + weight;
+                    if (newDistance < distances[to])
+                    {
+                        prev[to] = from;
+                        distances[to] = newDistance;
+                        q.Enqueue((to, newDistance));
+                    }
+                }
+            }
+
+            var path = GetPath(prev, distances, start, end);
+            return (distances, path);
+        }
+
+        private static int[] GetPath(int[] prev, int[] distances, int start, int end)
+        {
+            if (distances[end] == Int32.MaxValue)
+            { return Array.Empty<int>(); }
+
+            var steps = new List<int>();
+            var i = end;
+            while (i != start)
+            {
+                steps.Add(i);
+                i = prev[i];
+            }
+            steps.Add(start);
+            steps.Reverse();
+            return steps.ToArray();
         }
     }
 }
