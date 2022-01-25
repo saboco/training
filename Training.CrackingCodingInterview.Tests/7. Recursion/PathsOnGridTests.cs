3f7b3c158ca81d2b6010c0025d31ba19bc107f22
Santiago Botero Correa <sabotero@gmail.com>
2022-01-25 22:43:13 +0100

    some recursion

---
+++ Training.CrackingCodingInterview.Tests/7. Recursion/PathsOnGridTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace Training.CrackingCodingInterview.Tests._7._Recursion
+{
+    public class PathsOnGridTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(8, 3432)]
+        public void CountPathsTest(int n, int expectedCount)
+        {
+            var count = PathsOnGrid.CountPaths(n);
+            Assert.Equal(expectedCount, count);
+        }
+
+        [Theory]
+        [InlineData(1, new[] { 0 })]
+        [InlineData(0, new[] { 1 })]
+        [InlineData(2, new[] { 0, 0 }, new[] { 0, 0 })]
+        [InlineData(1, new[] { 0, 1 }, new[] { 0, 0 })]
+        [InlineData(6, new[] { 0, 0, 0 }, new[] { 0, 0, 0 }, new[] { 0, 0, 0 })]
+        [InlineData(2, new[] { 0, 0, 0 }, new[] { 0, 1, 0 }, new[] { 0, 0, 0 })]
+        [InlineData(3432
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            ,new[] { 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [InlineData(2592
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 1, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+            , new[] { 0, 0, 0, 0, 0, 0, 0, 0 })]
+        public void CountPathsWithObstaclesTest(int expectedCount, params int[][] grid)
+        {
+            var count = PathsOnGrid.CountPaths(grid);
+            Assert.Equal(expectedCount, count);
+        }
+    }
+}
---
+++ Training.CrackingCodingInterview/7. Recursion/PathsOnGrid.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.CrackingCodingInterview
+{
+    public class PathsOnGrid
+    {
+        public static int CountPaths(int n)
+        {
+            return CountPaths(n - 1, n - 1);
+        }
+
+        private static int CountPaths(int i, int j)
+        {
+            if (i == 0 && j == 0) { return 1; }
+            if (i == 0) { return CountPaths(0, j - 1); }
+            if (j == 0) { return CountPaths(i - 1, 0); }
+
+            return CountPaths(i, j - 1) + CountPaths(i - 1, j);
+        }
+
+        public static int CountPaths(int[][] g)
+        {
+            var n = g.Length;
+            return CountPaths(g, n - 1, n - 1);
+        }
+
+        private static int CountPaths(int[][] g, int i, int j)
+        {
+            if (i == 0 && j == 0 && g[0][0] == 0) { return 1; }
+            if (i == 0 && j == 0 && g[0][0] == 1) { return 0; }
+            if (i == 0 && g[0][j - 1] == 0) { return CountPaths(g, 0, j - 1); }
+            if (j == 0 && g[i - 1][0] == 0) { return CountPaths(g, i - 1, 0); }
+
+            var left = 0;
+            if (g[i][j - 1] == 0)
+            {
+                left = CountPaths(g, i, j - 1);
+            }
+            var up = 0;
+            if (g[i - 1][j] == 0)
+            {
+                up = CountPaths(g, i - 1, j);
+            }
+
+            return left + up;
+        }
+    }
+}
