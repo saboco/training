af2be78a21c376201928db2ba9a013368a880468
Santiago Botero Correa <sabotero@gmail.com>
2022-04-21 21:44:46 +0200

    more sorting and searching

---
+++ Training.CrackingCodingInterview.Tests/9. Sorting/FindElementInSortedMatrixTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Training.CrackingCodingInterview.Tests
+{
+    public class FindElementInSortedMatrixTests
+    {
+        readonly List<Func<int[][], int, (int, int)>> _action = new()
+        {
+            //FindElementInSortedMatrix.FindElement,
+            FindElementInSortedMatrix.FindElement2
+        };
+
+        [Theory]
+        [InlineData(
+            2,
+            1,
+            28,
+            new[] { 3, 5, 7, 9 },
+            new[] { 10, 12, 15, 20 },
+            new[] { 25, 28, 30, 35 },
+            new[] { 36, 37, 38, 40 })]
+        [InlineData(
+            2,
+            2,
+            26,
+            new[] { 1, 10, 20, 30 },
+            new[] { 2, 11, 25, 32 },
+            new[] { 5, 12, 26, 35 },
+            new[] { 8, 27, 38, 40 })]
+        [InlineData(
+            0,
+            0,
+            3,
+            new[] { 3, 5, 7, 9 },
+            new[] { 10, 12, 15, 20 },
+            new[] { 25, 28, 30, 35 },
+            new[] { 36, 37, 38, 40 })]
+        [InlineData(
+            3,
+            3,
+            40,
+            new[] { 1, 10, 20, 30 },
+            new[] { 2, 11, 25, 32 },
+            new[] { 5, 12, 26, 35 },
+            new[] { 8, 27, 38, 40 })]
+        [InlineData(
+            -1,
+            -1,
+            0,
+            new[] { 1, 10, 20, 30 },
+            new[] { 2, 11, 25, 32 },
+            new[] { 5, 12, 26, 35 },
+            new[] { 8, 27, 38, 40 })]
+        [InlineData(
+            0,
+            0,
+            1,
+            new[] { 1 })]
+        [InlineData(
+            -1,
+            -1,
+            1,
+            new int[0])]
+        public void FindElementTest(int expectedRow, int expectedColumn, int target, params int[][] m)
+        {
+            foreach (var action in _action)
+            {
+                var (actualRow, actualColumn) = action.Invoke(m, target);
+                Assert.Equal(expectedRow, actualRow);
+                Assert.Equal(expectedColumn, actualColumn);
+            }
+        }
+    }
+}
---
+++ Training.CrackingCodingInterview/9. Sorting/FindElementInSortedMatrix.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.CrackingCodingInterview
+{
+    public class FindElementInSortedMatrix
+    {
+        public static (int, int) FindElement(int[][] m, int target)
+        {
+            var rowIndex = -1;
+            var columnIndex = -1;
+            for (var r = 0; r < m.Length; r++)
+            {
+                var maxColIndex = m[r].Length - 1;
+                if (maxColIndex >= 0 && m[r][0] <= target && target <= m[r][maxColIndex])
+                {
+                    columnIndex = BinarySearch(m[r], target, 0, maxColIndex);
+                    if (columnIndex >= 0)
+                    {
+                        rowIndex = r;
+                        break;
+                    }
+                }
+            }
+            return (rowIndex, columnIndex);
+        }
+
+        public static (int, int) FindElement2(int[][] m, int target)
+        {
+            var rowIndex = -1;
+            var columnIndex = -1;
+
+            var row = 0;
+            var col = m[0].Length - 1;
+            while (row < m.Length && col >= 0)
+            {
+                if (m[row][col] == target)
+                {
+                    return (row, col);
+                }
+                else if (m[row][col] > target)
+                {
+                    col--;
+                }
+                else
+                {
+                    row++;
+                }
+            }
+
+            return (rowIndex, columnIndex);
+        }
+
+        private static int BinarySearch(int[] arr, int n, int lo, int hi)
+        {
+            if (lo > hi)
+            { return -1; }
+
+            var mid = (lo + hi) / 2;
+            if (arr[mid] == n)
+            { return mid; }
+
+            else if (n < arr[mid])
+            {
+                return BinarySearch(arr, n, lo, mid - 1);
+            }
+            else
+            {
+                return BinarySearch(arr, n, mid + 1, hi);
+            }
+        }
+    }
+}
