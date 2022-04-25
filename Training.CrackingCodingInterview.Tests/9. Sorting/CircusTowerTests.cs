92703409930efc2ed1d955aad128c4c52bb31597
Santiago Botero Correa <sabotero@gmail.com>
2022-04-25 22:41:38 +0200

    more in sorting and searching

---
+++ Training.CrackingCodingInterview.Tests/9. Sorting/CircusTowerTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Training.CrackingCodingInterview.Tests
+{
+    public class CircusTowerTests
+    {
+        [Theory]
+        [InlineData(
+            6,
+            new[] { 65, 70, 56, 75, 60, 68 },
+            new[] { 100, 150, 90, 190, 95, 110 })]
+        [InlineData(
+            5,
+            new[] { 65, 70, 56, 75, 60, 68 },
+            new[] { 90, 150, 90, 190, 95, 110 })]
+        public void MaxHeightTest(int expected, int[] heights, int[] weights)
+        {
+            Assert.Equal(heights.Length, weights.Length);
+            var people = new List<(int, int)>();
+            for (var i = 0; i < weights.Length; i++)
+            {
+                people.Add((heights[i], weights[i]));
+            }
+            var actual = CircusTower.MaxHeight(people.ToArray());
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Training.CrackingCodingInterview/9. Sorting/CircusTower.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Training.CrackingCodingInterview
+{
+    public class CircusTower
+    {
+        public static int MaxHeight((int, int)[] people)
+        {
+            var comparer = Comparer<(int, int)>.Create((a, b) => a.Item1 == b.Item1 ? 0 : a.Item1 < b.Item1 ? -1 : 1);
+            Array.Sort(people, comparer);
+
+            var towers = new List<(int, int)[]>();
+            if (!WeightIsInOrder(people))
+            {
+                MaxHeight(people, 0, new List<(int, int)>(), towers);
+            }
+            else
+            {
+                towers.Add(people);
+            }
+            return towers.Max(t => t.Length);
+        }
+
+        private static void MaxHeight((int, int)[] people, int index, List<(int, int)> current, List<(int, int)[]> towers)
+        {
+            if (index == people.Length)
+            {
+                if (WeightIsInOrder(current))
+                {
+                    towers.Add(current.ToArray());
+                }
+            }
+
+            for (var i = index; i < people.Length; i++)
+            {
+                current.Add(people[i]);
+                MaxHeight(people, i + 1, current, towers);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+
+        private static bool WeightIsInOrder(IList<(int, int)> people)
+        {
+            var (_, prev) = people[0];
+            for (var i = 1; i < people.Count; i++)
+            {
+                var (_, current) = people[i];
+                if (prev > current)
+                {
+                    return false;
+                }
+                prev = current;
+            }
+            return true;
+        }
+    }
+}
